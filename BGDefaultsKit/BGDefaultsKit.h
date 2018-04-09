424d7ff97fbf25972a202397fce1bdb43b2d380b
Yanyan <wangdoubleyan@qq.com>
2018-04-09 17:59:41 +0800

    init

---
+++ BGDefaultsKit/BGDefaultsKit.h
@@ -0,0 +1,19 @@
+//
+//  BGDefaultsKit.h
+//  BGDefaultsKit
+//
+//  Created by yanyan on 2018/4/9.
+//  Copyright © 2018年 PotatoYY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BGDefaultsKit.
+FOUNDATION_EXPORT double BGDefaultsKitVersionNumber;
+
+//! Project version string for BGDefaultsKit.
+FOUNDATION_EXPORT const unsigned char BGDefaultsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BGDefaultsKit/PublicHeader.h>
+
+
